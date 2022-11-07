89eae24e9d7f6fd4a3d47c1e841017b5c81a8a29
JukKie-ai <tabunojn262@gmail.com>
2022-11-08 01:19:50 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [add, setAdd] = useState([]);
+
+  function getData(val) {
+    setData(val.target.value);
+    console.warn(data);
+  }
+
+  const addNumber = (e) => {
+    add.push(e.target.value);
+    setAdd(add);
+    console.log(add);
+  };
+
+  function compare() {
+    let text = add.join("");
+
+    if (data === text) {
+      alert("Congratulations! Successfully Unlocked!");
+      setAdd([]);
+    } else {
+      alert("Try again! Passkey is " + data + ", Combo inputted is " + text);
+      setAdd([]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      <input
+        type="text"
+        className="input"
+        name="input"
+        placeholder="Pass Key"
+        onChange={getData}
+      ></input>
+      <button onClick={(e) => addNumber(e)} value="1">
+        1
+      </button>
+      <button onClick={(e) => addNumber(e)} value="2">
+        2
+      </button>
+      <button onClick={(e) => addNumber(e)} value="3">
+        3
+      </button>
+      <button onClick={(e) => addNumber(e)} value="4">
+        4
+      </button>
+      <button onClick={(e) => addNumber(e)} value="5">
+        5
+      </button>
+      <button onClick={(e) => addNumber(e)} value="6">
+        6
+      </button>
+      <button onClick={(e) => addNumber(e)} value="7">
+        7
+      </button>
+      <button onClick={(e) => addNumber(e)} value="8">
+        8
+      </button>
+      <button onClick={(e) => addNumber(e)} value="9">
+        9
+      </button>
+      <button className="span-three" id="green" onClick={compare}>
+        CHECK
+      </button>
     </div>
   );
 }
